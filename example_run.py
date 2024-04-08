6b44c763f3ee2d9b5ffe16a3c3d69b42140d9b6d
LRoga <l.q.roga@uu.nl>
2024-04-08 21:51:38 +0200

    memory leak fix

--- example_run.py
+++ example_run.py
@@ -2,12 +2,12 @@ import raster_extraction_functions as ref
 
 if __name__ == "__main__":
     ref.extract_values(
-        input_csv=r'C:\path\to\input.csv', #file at least 2 coordinate columns calld X and Y
-        raster_folder=r'C:\path\to\rasterfolder', #directory where rasters to be used are saved, no sub dir
-        output_csv=r'C:\path\to\output.csv', #output file to be created
-        in_crs='EPSG:28992', #crs of input csv
-        raster_crs='EPSG:3035', #crs of rasters to be used (EPSG:3035 in case of EXPANSE rasters)
-        sep = ';', #column separator in input file, output will always be semi-colon
-        decimal=',', #decimal separator in input file
-        writemethod='concat' #method to write output to csv, "concat"  is fast but memory-intensive, "rows" is slow but requires no extra memory"
+        input_csv=r'C:\path\to\input.csv', # file with at least 2 coordinate columns called "X" and "Y".
+        raster_folder=r'C:\path\to\rasterfolder', # directory where rasters to be used are saved, does not read subdirs.
+        output_csv=r'C:\path\to\output.csv', # output file to be created.
+        in_crs='EPSG:28992', # Coordinate Reference System of input csv.
+        raster_crs='EPSG:3035', # Coordinate Reference System of rasters to be used (EPSG:3035 in case of EXPANSE rasters).
+        sep = ';', # column separator in input file, output will always be semi-colon.
+        decimal=',', # decimal separator in input file.
+        writemethod='concat' # method to write output to csv, "concat"  is fast but memory-intensive, "rows" is slow but requires no extra memory.
     )
--- raster_extraction_functions.py
+++ raster_extraction_functions.py
@@ -34,12 +34,11 @@ def _update_progress(result, progress_counter, total_rasters):
     """Callback function to update progress."""
     progress_counter.value += 1
     print(f"Processed {progress_counter.value}/{total_rasters} rasters")
-    if progress_counter.value % 100 == 0:
-        gc.collect()
+    # if progress_counter.value % 100 == 0:
+    #     gc.collect()
 
-def process_raster(args):
-    """Modified worker function to process a single raster file in chunks."""
-    df, raster_folder, raster_file = args
+def process_raster(x_coords, y_coords,raster_folder,raster_file):
+    """Worker function to process a single raster file in chunks."""
     raster_path = os.path.join(raster_folder, raster_file)
 
     dataset = gdal.Open(raster_path)
@@ -52,15 +51,15 @@ def process_raster(args):
     chunk_width, chunk_height = _calculate_chunk_size(dataset)
 
     # Initialize array to hold values
-    values = np.full(df.shape[0], np.nan, dtype='float32')  
+    values = np.full(len(x_coords), np.nan, dtype='float32')  
 
     for y_offset in range(0, dataset.RasterYSize, chunk_height):
         rows_to_read = min(chunk_height, dataset.RasterYSize - y_offset)
         raster_array = band.ReadAsArray(0, y_offset, chunk_width, rows_to_read).astype('float32')
 
         # Calculate global x and y indices for the dataframe points
-        x_indices = np.floor((df['X'] - geotransform[0]) / geotransform[1]).astype(int)
-        y_indices = np.floor((df['Y'] - geotransform[3]) / geotransform[5]).astype(int) - y_offset
+        x_indices = np.floor((x_coords - geotransform[0]) / geotransform[1]).astype(int)
+        y_indices = np.floor((y_coords - geotransform[3]) / geotransform[5]).astype(int) - y_offset
 
         # Mask for points falling within the current chunk
         mask = (x_indices >= 0) & (x_indices < chunk_width) & (y_indices >= 0) & (y_indices < rows_to_read)
@@ -69,7 +68,7 @@ def process_raster(args):
         values[chunk_mask] = raster_array[y_indices[chunk_mask], x_indices[chunk_mask]]
 
     column_name = os.path.splitext(raster_file)[0]
-    dataset = None
+    del dataset
     return column_name, values
 
 def extract_values(input_csv, raster_folder, output_csv, in_crs, raster_crs, writemethod='concat',sep= ';',decimal='.'):
@@ -92,12 +91,13 @@ def extract_values(input_csv, raster_folder, output_csv, in_crs, raster_crs, wri
 
     manager = Manager()
     progress_counter = manager.Value('i', 0)
+    x_coords = df['X'].values
+    y_coords = df['Y'].values
 
     results = []
     with Pool(processes=cpu_count()) as pool:
         for raster_file in raster_files:
-            args = (df.copy(), raster_folder, raster_file)
-            result = pool.apply_async(process_raster, args=(args,), callback=lambda result: _update_progress(result, progress_counter, total_rasters))
+            result = pool.apply_async(process_raster, args=(x_coords,y_coords,raster_folder,raster_file), callback=lambda result: _update_progress(result, progress_counter, total_rasters))
             results.append(result)
         pool.close()
         pool.join()
@@ -127,7 +127,7 @@ def extract_values(input_csv, raster_folder, output_csv, in_crs, raster_crs, wri
     #Writing to CSV row by row without concatenating dataframes. This is memory efficient (no in-memory concatenation), but slower.
     elif writemethod == 'rows':
         print('Writing dataframe to csv...')
-        assert len(df) == len(new_columns_df), "DataFrames must have the same number of rows" # Ensure both DataFrames have the same number of rows
+        assert len(df) == len(new_columns_df), f"DataFrames must have the same number of rows. Original: {len(df)}, new values: {len(new_columns_df)}" # Ensure both DataFrames have the same number of rows
 
         with open(output_csv, 'w', newline='') as csvfile:
             headers = df.columns.tolist() + new_columns_df.columns.tolist()
